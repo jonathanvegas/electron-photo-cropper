4b566b8a8db050e4d55c037224d72f985f810518
Jonathan Vegas <jonathanvegas@gmail.com>
2022-08-23 11:00:59 -0400

    image cropping but not saving

--- src/renderer/components/Photo.tsx
+++ src/renderer/components/Photo.tsx
@@ -1,3 +1,40 @@
+import { useState } from "react";
+import Cropper from 'react-easy-crop';
+import { readFile } from "../../helpers/images";
+
 export default function Photo() {
-  return <h1>Photo Cropper goes here</h1>
+  const [imageSrc, setImageSrc] = useState(null); //file data
+  const [crop, setCrop] = useState({ x:0, y:0 });
+  const [zoom, setZoom] = useState(1);
+  // const [filename, setFilename] = useState(null); //file address
+  const handleFileChange = async (e: any) => {
+    if (e.target.files && e.target.files.length){
+      //we got a file...
+      const file = e.target.files[0];
+      // setFilename(file.path);
+      //get the image data from the file
+      const imageData: any = await readFile(file);
+      //setImageSrc to that image data
+      setImageSrc(imageData);
+    }
+  }
+  if(!imageSrc) {
+    return (
+      <>
+        <h1>Please chose photo to crop</h1>
+        <input type="file" accept="image/*" onChange={handleFileChange}/>
+      </>
+    )
+  }
+  return(
+    <>
+      <Cropper
+        image={imageSrc}
+        crop={crop}
+        zoom={zoom}
+        onCropChange={setCrop}
+        onZoomChange={setZoom}
+      />
+    </>
+  )
 }
